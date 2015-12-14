33bc1693bd5986295fdf9894b935276c4bf0f46f
jarekjaro <krzeminskijarek@gmail.com>
2015-12-14 22:27:25 +0100

    message formatting for listeners fixed

--- src/com/luxoft/bankapp/service/EmailNotificationListener.java
+++ src/com/luxoft/bankapp/service/EmailNotificationListener.java
@@ -3,6 +3,7 @@ package com.luxoft.bankapp.service;
 import com.luxoft.bankapp.model.Client;
 
 public class EmailNotificationListener implements ClientRegistrationListener {
+
     @Override
     public void onClientAdded(Client client) {
         System.out.printf("Notification email for a client %11s to be sent.\n", client);
--- src/com/luxoft/bankapp/service/PrintClientListener.java
+++ src/com/luxoft/bankapp/service/PrintClientListener.java
@@ -3,6 +3,7 @@ package com.luxoft.bankapp.service;
 import com.luxoft.bankapp.model.Client;
 
 public class PrintClientListener implements ClientRegistrationListener {
+
     @Override
     public void onClientAdded(Client client) {
         System.out.printf("Client %11s added.\n",client);
