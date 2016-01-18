a58d66f5954d9b9f86ac55cd7aa706e9021e9c12
jarekjaro <krzeminskijarek@gmail.com>
2016-01-18 15:16:43 +0100

    corrected a test and added testFile

--- src/main/java/com/luxoft/bankapp/tests/BankServerThreadedTest.java
+++ src/main/java/com/luxoft/bankapp/tests/BankServerThreadedTest.java
@@ -1,13 +1,29 @@
-package tests;
+package com.luxoft.bankapp.tests;
 
+import com.luxoft.bankapp.model.Bank;
+import com.luxoft.bankapp.service.BankApplication;
 import com.luxoft.bankapp.service.BankClientMock;
+import com.luxoft.bankapp.service.BankServerThreaded;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
 public class BankServerThreadedTest {
+    Thread threadedServerThread = new Thread();
+
+    @Before
+    public void runServer() throws IOException {
+        Bank bank1 = new Bank();
+        BankApplication.initialize(bank1);
+        BankServerThreaded serverThreaded = new BankServerThreaded(bank1);
+        serverThreaded.run();
+    }
+
     @Test
-    public void BankClientMockTest() {
+    public void BankClientMockTest() throws InterruptedException {
         BankClientMock clientMock = new BankClientMock();
         for (int i = 0; i < 1000; i++) {
             Thread newThread = new Thread(clientMock);
