0e2428956654475315bb39938e400290c9b3a644
jarekjaro <krzeminskijarek@gmail.com>
2016-01-31 00:49:46 +0100

    ex 13 started

---
+++ src/main/java/com/luxoft/bankapp/servlets/WelcomeServlet.java
@@ -0,0 +1,17 @@
+package com.luxoft.bankapp.servlets;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class WelcomeServlet extends HttpServlet {
+    private static final long serialVersionUID = -6703624962716075443L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletOutputStream servletOutputStream = response.getOutputStream();
+        servletOutputStream.println("Hello! I'm ATM <br>");
+        servletOutputStream.println("<a href='login.html'>Login</a>");
+    }
+}
